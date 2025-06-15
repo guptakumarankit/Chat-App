ea4febb7840c316e88080947228c24fd3f459419
AnkitGupta <ankit.kr778389@gmail.com>
2025-06-15 21:32:58 +0530

    HomePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import {Route , Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import ProfilePage from './pages/ProfilePage'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   return (
-    <div className= "bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
       <Routes>
-        <Route path ='/' element = {<HomePage/>}>HomePage</Route>
-        <Route path = "/login" element = {<LoginPage/>}></Route>
-        <Route path = "/profile" element = {<ProfilePage/>}></Route>
+        <Route path="/" element={<HomePage />}>
+          HomePage
+        </Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/profile" element={<ProfilePage />}></Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
